dfbebb1532a76db711e669a0eedb1c934a1ab658
xuantain <xuantain@gmail.com>
2015-11-07 12:46:14 +0700

    create new components

---
+++ public/src/jsx/components/AdminContent.jsx
@@ -0,0 +1,21 @@
+'use strict';
+
+var React = require('react');
+
+module.exports = React.createClass({
+	render: function() {
+		return (
+			<div id='page-wrapper'>
+				<div className='row'>
+					<div className='col-lg-12'>
+						<h1 className='page-header'>Dashboard</h1>
+					</div>
+					{/*<!-- /.col-lg-12 -->*/}
+				</div>
+				{/*<!-- /.row -->*/}
+
+			</div>
+			//<!-- /#page-wrapper -->
+		);
+	}
+});
