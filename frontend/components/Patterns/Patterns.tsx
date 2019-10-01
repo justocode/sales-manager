7fb14cdc9d49a0ff5cfdc288617880c0bf4668b0
xuantain <xuantain@gmail.com>
2019-10-01 15:48:19 +0700

    Patterns page : Mange pattern is used & relevant colors

--- frontend/components/Patterns/Patterns.tsx
+++ frontend/components/Patterns/Patterns.tsx
@@ -1,140 +1,207 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import _ from 'lodash';
-import Head from 'next/head';
-import { useDispatch, useSelector } from 'react-redux';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import IconButton from '@material-ui/core/IconButton';
-import { makeStyles, Theme, useTheme } from '@material-ui/core/styles';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import InfoIcon from '@material-ui/icons/Info';
-import { ProductSortKeys } from '../../types/shopify.type';
-import { ProductsState } from '../../stores/products.slice';
+import { createStyles, makeStyles, useTheme, Theme } from '@material-ui/core/styles';
+import clsx from 'clsx';
+
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
+
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+import CheckIcon from '@material-ui/icons/Check';
+
+// Colors
+import { lightGreen } from '@material-ui/core/colors';
+
+// Components
 import Layout from '../Layout/Layout';
-import withLayout from '../../hocs/withLayout';
+import IChip from '../common/IChip';
+
+// Models
+import { PATTERN } from '../../types/amz-shirt.type';
+
+import { services } from '../../services';
 import { utils } from '../../utils';
 
-interface Props {
-  query: {
-    query: string;
-    reverse: boolean;
-    sortKey: ProductSortKeys;
-    sortIndex: number;
-  };
-}
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    pattern: {
+      position: 'relative',
+      marginLeft: 10,
+      marginRight: 5,
+      maxWidth: 345,
+      minWidth: 200,
+      width: 200,
+      height: 200,
+      display: 'inline-flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    patternDeactived: {
+      backgroundColor: 'gray'
+    },
+    patternInfo: {
+      paddingLeft: 20
+    },
+    patternColor: {
+      position: 'relative',
+      paddingTop: 15,
+      paddingBottom: 15,
+      textAlign: 'center',
+      cursor: 'pointer',
+    },
+    patternColorAvailable: {
+      opacity: 1
+    },
+    media: {
+      height: 140,
+      backgroundSize: 'contain'
+    },
+    expandDetail: {
+      paddingLeft: 0,
+      paddingRight: 0,
+    },
+    checkedIcon: {
+      display: 'none'
+    },
+    checked: {
+      display: 'inline-block',
+      position: 'absolute',
+      top: 5,
+      left: 5,
+      background: lightGreen[500]
+    },
+    colorChecked: {
+      left: 60,
+    }
+  })
+);
 
-const useStyles = makeStyles((theme: Theme) => ({
-  products: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'flex-start',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper
-  },
-  gridList: {
-    // width: '100%'
-  },
-  icon: {
-    color: 'rgba(255, 255, 255, 0.54)'
-  },
-  header: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    marginBottom: 30,
-    alignItems: 'center'
-  },
-  empty: {
-    display: 'block',
-    width: '100%',
-    textAlign: 'center'
-  },
-  loader: {
-    display: 'block',
-    width: '100%',
-    textAlign: 'center',
-    marginBottom: 30
-  }
-}));
+const defaultPatterns = () => {
+  let returnedData = {};
 
-function Patterns({ query }: Props) {
-  const dispatch = useDispatch();
-  const { firstPage, nextPage, data }: ProductsState = useSelector(({ products }) => products);
-  const cursor = data ? (data.edges.length ? _.last(data.edges).cursor : '') : '';
-  const hasNextpage = data ? data.pageInfo.hasNextPage : false;
+  Object.keys(services.patternStore.patterns).map(key => {
+    returnedData[key] = {
+      isUsed: true,
+      colors: services.patternStore.patterns[key],
+    };
+  });
 
+  return returnedData;
+};
+
+const ManagePatterns = (props: any) => {
   const theme = useTheme();
   const classes = useStyles(theme);
-  let gridListCols = 4;
+  const [patterns, setPatterns] = utils.useStateWithLocalStorage('patterns', defaultPatterns);
 
-  if (useMediaQuery(theme.breakpoints.down('md'))) {
-    gridListCols = 3;
+  function togglePatternColor(patternName: string, patternColor: any) {
+    setPatterns((patterns: any) => {
+      let pattern = patterns[patternName];
+      pattern.colors.map((patternColorItem: any) => {
+        if (patternColorItem.hex === patternColor.hex) {
+          patternColorItem.isDefault = !patternColorItem.isDefault;
+        }
+      });
+
+      return { ...patterns, [patternName]: pattern };
+    });
   }
 
-  if (useMediaQuery(theme.breakpoints.down('sm'))) {
-    gridListCols = 2;
-  }
+  function togglePattern(e: any, patternName: string) {
+    e.stopPropagation();
 
-  if (useMediaQuery(theme.breakpoints.down('xs'))) {
-    gridListCols = 1;
+    setPatterns((patterns: any) => {
+      let pattern = patterns[patternName];
+      pattern.isUsed = !pattern.isUsed;
+
+      return { ...patterns, [patternName]: pattern };
+    });
   }
 
   return (
     <Layout>
-      {firstPage.loading && (
-        <div className={classes.loader}>
-          <CircularProgress size={24} />
-        </div>
-      )}
+      {
+        Object.keys(patterns).map((key: string) => {
+          const pattern: any = _.get(patterns, [key, 'colors', 0]);
 
-      {firstPage.error && <p>{firstPage.error.message}</p>}
-
-      {data && (
-        <>
-          <div className={classes.products}>
-            <Head>
-              <title>Products - Sales manager</title>
-            </Head>
-            <GridList className={classes.gridList} cellHeight={500} cols={gridListCols} spacing={30}>
-              {data.edges.map(({ node }) => {
-                const images = node.images.edges;
-                const imageSrc = images.length
-                  ? images[0].node.transformedSrc
-                  : 'http://www.netum.vn/public/default/img/icon/default-product-image.png';
-                const altText = images.length ? images[0].node.altText : '';
-
-                return (
-                  <GridListTile key={node.handle}>
-                    <img src={imageSrc} alt={altText} />
-                    <GridListTileBar
-                      title={node.title}
-                      subtitle={<span>${node.priceRange.minVariantPrice.amount}</span>}
-                      actionIcon={
-                        <IconButton
-                          className={classes.icon}
-                          onClick={() =>
-                            utils.link({
-                              path: '/product',
-                              params: {
-                                handle: node.handle
-                              }
-                            })
-                          }
-                        >
-                          <InfoIcon />
-                        </IconButton>
-                      }
-                    />
-                  </GridListTile>
-                );
-              })}
-            </GridList>
-          </div>
-        </>
-      )}
+          return (
+            <ExpansionPanel key={'pattern-' + key}>
+              <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls={'pattern-content-' + key}
+                id={'pattern-header-' + key}
+                className={clsx({ [classes.patternDeactived]: !patterns[key].isUsed })}
+              >
+                <Card className={classes.pattern} onClick={e => togglePattern(e, key)}>
+                  <CardActionArea>
+                    <CardMedia className={classes.media} image={pattern.fileSrc} title={pattern.name} />
+                  </CardActionArea>
+                  <CheckIcon
+                    className={clsx(classes.checkedIcon, {
+                      [classes.checked]: patterns[key].isUsed
+                    })}
+                  />
+                </Card>
+                <div className={classes.patternInfo}>
+                  <div>
+                    <Typography variant="h5" component="h5">
+                      {key}
+                    </Typography>
+                  </div>
+                  <div>
+                    <Typography variant="h5" component="h5">
+                      Default Colors:
+                    </Typography>
+                    {_.get(patterns, [key, 'colors']).map((patternColor: any, patternColorIndex: number) => (
+                      <IChip
+                        key={'patternColorItem-' + patternColor.hex}
+                        label={patternColor.name}
+                        color={patternColor}
+                        onClick={[togglePatternColor, key, patternColor]}
+                        className={clsx({[classes.patternColorAvailable]: patternColor.isDefault})}
+                      />
+                    ))}
+                  </div>
+                </div>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails className={classes.expandDetail}>
+                <Grid container>
+                  {_.get(patterns, [key, 'colors']).map((patternColor: any, patternColorIndex: number) => {
+                    return (
+                      <Grid
+                        className={classes.patternColor}
+                        key={'patternColor-' + patternColor.fileName}
+                        item xs={4} sm={3} lg={2}
+                      >
+                        <img
+                          className={classes.media}
+                          src={patternColor.fileSrc}
+                          alt={patternColor.fileName}
+                          onClick={e => {togglePatternColor(key, patternColor)}}
+                        />
+                        <CheckIcon
+                          className={clsx(classes.checkedIcon, classes.colorChecked, {
+                            [classes.checked]: patternColor.isDefault
+                          })}
+                        />
+                      </Grid>
+                    );
+                  })}
+                </Grid>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          );
+        })
+      }
     </Layout>
   );
-}
+};
 
-export default withLayout(Patterns);
+export default ManagePatterns;
